82ded1d33476fcf63672a5f9d33a8049b47ca892
Travis <tlneondo@gmail.com>
2025-07-17 11:45:46 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#given working directory which contains the game
+
+
+#pull directory name
+
+
+#match to appmanifest file
+
+
+#match what steam api depos are installed
+
+
+
+#filter out pwd level .dll files should be in the folder
+
+
+#compare to what dll files are in the folder currently 
+
+
+#query user which dlls need to be overridden
+
+
+#write DLL overrides to registry or write WINEDLLOVERRIDES env variable in steam launch options
