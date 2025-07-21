885b34ca7fb8a4c034d06dd3cd225e66aa6bcb28
tlneondo <tlneondo@gmail.com>
2025-07-21 16:56:35 -0400

    further splitting

--- main.py
+++ main.py
@@ -3,42 +3,68 @@ import sys
 from pathlib import Path
 from utils import keyvalues
 
-#expect directory, but if null pass the cwd
+import requests
 
-if len(sys.arv = 2):
+
+#https://medium.com/codex/scraping-information-of-all-games-from-steam-with-python-6e44eb01a299
+
+#expect directory, but if null pass the cwd
+gamedir = None
+
+
+if len(sys.argv = 2):
     gamedir = sys.argv[0]
-elif(sys.arv = 1):
+elif len(sys.argv = 1):
     gamedir = os.getcwd()
 else: exit
-     
 
 
+steamGameID = matchSteamID(gamedir)
 
+def matchSteamID(title):
+    """
+    Matches the Steam ID of a game title.
+    
+    THIS IS COPILOT GENERATED WHAT THE FUCK, this is the devil
 
-#empty slot for acf file
-acffile = None
-
-folderDlls = None
+    :param title: The title of the game to match.
+    :return: The Steam ID if found, otherwise None.
+    """
+    url = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        for app in data['applist']['apps']:
+            if app['name'].lower() == title.lower():
+                return app['appid']
+    return None
 
 
 def matchManifest():
+    os.chdir(gamedir)
+    oc.chdir("../../")
+    acfdir = os.getcwd()
 
-    os.chdir('../../')
 
-    for o in path.rglob('*.acf')
-        if o.is_file():
-            text = o.read_text()
-            if gamedir in text:
-                println("acf found")
-                acffile = o
+
+    files = [x for x in os.listdir(acfdir) if x.endswith(".acf")]
+
+    for file in files:
+        if (file.find)
+
+
+
+        
+
+
+
+
+
+
 
 
 def matchSteamDepots():
-
-        os.chdir(gamedir)
-
-    for o in path.rglob(".dll")
-        if o.is_file():
 
 
 def getVanillaDLLList():
